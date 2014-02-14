21cc6e57eb6bea652080ac3093ae7c82cbc4c425
Denis <plotnic@hotbox.ru>
2014-02-14 15:24:52 +0400

    the algorithm of calculating of assignments has been changed

--- ImageSearchScript.m
+++ ImageSearchScript.m
@@ -1,5 +1,4 @@
-%% new image processing
-% codebook calculation
+%% codebook calculation
 % % % % searchImagePath = '/mnt/Images/Holidays/';
 % % % % codebookImagePath = '/mnt/Images/1/';
 % % % % codebookSize = 64;
@@ -12,8 +11,8 @@
 % % % % searchFiles = getFilesOnly(searchImagePath);
 % % % % searchFilesAmount = numel(searchFiles);
 % % % % imageDataBase = cell(searchFilesAmount, 1);
-
-%% vlad calculation
+% % % % 
+% % % % %% vlad calculation
 % % % % tic;
 % % % % for i = 1:searchFilesAmount
 % % % %     imageDataBase{i} = getFileValdStruct( [searchImagePath searchFiles(i).name], []);
@@ -24,9 +23,23 @@
 
 
 %% query performing
-indexToSearch = 1471;
-
-queryPath = imageDataBase{indexToSearch}.fileName;
+indexToSearch = 47;
 resultsAmount = 15;
 
+queryPath = imageDataBase{indexToSearch}.fileName;
+%queryPath = '/home/plotnikov/korall.jpg';
+
+
 searchImage(queryPath, imageDataBase, codebook, resultsAmount);
+
+
+
+%% function testing
+% % % % % queryPath = '/home/plotnikov/korall.jpg';
+% % % % % % % % % % % 
+% % % % % % % % % % % v = getVlad(queryPath, codebook);
+% % % % % 
+% % % % %   sifts = getSiftsFromFile(queryPath);
+% % % % %   
+% % % % %   vlad = getVlad(queryPath, codebook);
+
--- getVlad.m
+++ getVlad.m
@@ -3,6 +3,8 @@ function [ vlad ] = getVlad( fileName, codebook)
 
   sifts = getSiftsFromFile(fileName);
   assignment = vl_alldist2(sifts, codebook)';
+  colSums = sum(assignment, 1);
+  assignment = assignment./repmat(colSums, size(assignment, 1), 1);
   vlad = vl_vlad(sifts, codebook, assignment);
 end
 
